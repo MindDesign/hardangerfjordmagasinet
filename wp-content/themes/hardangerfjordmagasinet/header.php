c689e9057a9d90d7963af7821bab4921850535ee
Tommy Eliassen <pusle@puslecom>
2021-03-21 10:31:43 +0100

    Theme progress

--- wp-content/themes/hardangerfjordmagasinet/header.php
+++ wp-content/themes/hardangerfjordmagasinet/header.php
@@ -21,27 +21,25 @@
 <body <?php body_class('p-0 lg:p-4 xl:p-6'); ?>>
 <?php wp_body_open(); ?>
 
-    <header class="max-w-5xl" id="site-header">
-        <div class="mb-8 w-full h-32 bg-green-400" id="site-header-banner"> Banner </div>
+    <header class="max-w-5xl relative" id="site-header">
+        <div class="mb-8 w-full h-32 bg-primary" id="site-header-banner"> Banner </div>
 
         <div class="mb-8 flex justify-between items-center">
-            <img class="w-96 h-auto" src="<?php echo get_stylesheet_directory_uri() . '/img/hardangerfjordmagasinet-logo.svg' ?>" alt="">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="">
+                <img class="w-96 h-auto" src="<?php echo get_stylesheet_directory_uri() . '/img/hardangerfjordmagasinet-logo.svg' ?>" alt="">
+            </a>
             <div class="flex flex-col items-end">
                 <div class="px-4 py-2 mb-3 w-44 bg-gray-100">Søk</div>
                 <nav class="">
-                    <a href="#" class="pr-4 text-xl text-green-400 uppercase">Artikler</a>
-                    <a href="#" class="pr-4 text-xl text-green-400 uppercase">Annonsere</a>
-                    <a href="#" class="pr-4 text-xl text-green-400 uppercase">Om oss</a>
-                    <a href="#" class="text-xl text-green-400 uppercase">Kontakt oss</a>
+                    <a href="#" class="pr-4 text-xl text-primary uppercase">Artikler</a>
+                    <a href="#" class="pr-4 text-xl text-primary uppercase">Annonsere</a>
+                    <a href="#" class="pr-4 text-xl text-primary uppercase">Om oss</a>
+                    <a href="#" class="text-xl text-primary uppercase">Kontakt oss</a>
                 </nav>
             </div>
         </div>
-    </header>
 
-    <section class="max-w-5xl relative" id="frontpage-featured">
-        <img src="https://via.placeholder.com/970x370.png" class="w-full h-auto" alt="">
-        <div class="max-w-xl absolute left-5 bottom-5">
-            <h1 class="mb-3 text-shadow text-4xl text-white">Overskrift på artikkel</h1>
-            <p class="text-white text-xl">Ingress på artikkel kommer her. Ingress på artikkel kommer her. Ingress på artikkel kommer her. Ingress på artikkel kommer her.</p>
+        <div class="absolute top-0 -right-80 w-72 bg-primary h-96">
+            Banner
         </div>
-    </section>
+    </header>
--- wp-content/themes/hardangerfjordmagasinet/tailwind.config.js
+++ wp-content/themes/hardangerfjordmagasinet/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          lightest: '#8AA43C',
+          lighter: '#8AA43C',
+          light: '#8AA43C',
+          DEFAULT: '#8AA43C',
+          dark: '8AA43C',
+          darker: '#8AA43C',
+          darkest: '#8AA43C'
+        },
+      }
+    }
   },
   variants: {
     extend: {},
